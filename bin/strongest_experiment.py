0a5feecb49e3060c2f54e02a0df815e9aabfa1f8
Joshua Meyer <josh.meyer@avantcredit.com>
2016-05-30 09:13:58 -0500

    increased sample size back up

--- bin/strongest_experiment.py
+++ bin/strongest_experiment.py
@@ -3,9 +3,9 @@ from lib.player_generators.base_player_generator import BasePlayerGenerator
 from lib.matches.strongest_wins_match import StrongestWinsMatch
 from lib.tiered_leagues.base_tiered_league import BaseTieredLeague
 
-pb = BasePlayerGenerator(1000)
+pb = BasePlayerGenerator(10000)
 tl = BaseTieredLeague(pb, StrongestWinsMatch)
 
 for i in range(0, 100):
-  tl.do_an_iteration()
+    tl.do_an_iteration()
 tl.print_summary()
--- bin/weakest_experiment.py
+++ bin/weakest_experiment.py
@@ -3,9 +3,9 @@ from lib.player_generators.base_player_generator import BasePlayerGenerator
 from lib.matches.weakest_loses_match import WeakestLosesMatch
 from lib.tiered_leagues.base_tiered_league import BaseTieredLeague
 
-pb = BasePlayerGenerator(1000)
+pb = BasePlayerGenerator(10000)
 tl = BaseTieredLeague(pb, WeakestLosesMatch)
 
 for i in range(0, 100):
-  tl.do_an_iteration()
+    tl.do_an_iteration()
 tl.print_summary()
