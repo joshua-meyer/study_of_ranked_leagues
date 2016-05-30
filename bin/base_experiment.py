0498a5459fcc51ab9fff38111a812e9207a35c04
Joshua Meyer <josh.meyer@avantcredit.com>
2016-05-30 08:46:03 -0500

    reduced sample size

--- bin/base_experiment.py
+++ bin/base_experiment.py
@@ -3,7 +3,7 @@ from lib.player_generators.base_player_generator import BasePlayerGenerator
 from lib.matches.base_match import BaseMatch
 from lib.tiered_leagues.base_tiered_league import BaseTieredLeague
 
-pb = BasePlayerGenerator(10000)
+pb = BasePlayerGenerator(1000)
 tl = BaseTieredLeague(pb, BaseMatch)
 
 for i in range(0, 100):
--- bin/team_total_experiment.py
+++ bin/team_total_experiment.py
@@ -3,7 +3,7 @@ from lib.player_generators.base_player_generator import BasePlayerGenerator
 from lib.matches.team_total_match import TeamTotalMatch
 from lib.tiered_leagues.base_tiered_league import BaseTieredLeague
 
-pb = BasePlayerGenerator(10000)
+pb = BasePlayerGenerator(1000)
 tl = BaseTieredLeague(pb, TeamTotalMatch)
 
 for i in range(0, 100):
